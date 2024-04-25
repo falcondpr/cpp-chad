dff7e8d5550c1b55433f0ed5b62375bac17f6f01
Fernando Villalba <21fervillalbag@gmail.com>
2024-04-25 01:18:41 -0400

    feat: starting with stairway-to-heaven

---
+++ stairway-to-heaven/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  /*
+    std::cout << "Fernando" << std::endl;
+    std::cout << "Villalba" << std::endl;
+  */
+  
+  std::cout << "Fernando" << std::endl;
+  std::cout << "Villalba" << std::endl;
+
+  // std:: cout << "Number1";
+  // std:: cout << "Number2";
+  return 0;
+}
